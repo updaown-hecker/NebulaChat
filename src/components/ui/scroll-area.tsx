152b927a3f09e272dc434dbf9272c09a517a1e78
Updaown Youtube <updaowny@gmail.com>
2025-06-07 22:26:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import * as React from "react"
-import *import type { ForwardedRef } from 'react';
+import type { ForwardedRef } from 'react';
 import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
