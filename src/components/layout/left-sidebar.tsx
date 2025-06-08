d04c6e6b8ec35e0f09b264967f90f5e84b8b7881
Updaown Youtube <updaowny@gmail.com>
2025-06-08 04:47:17 +0000

    oop just figured out that you can't see the friends tab on mobile

--- src/components/layout/left-sidebar.tsx
+++ src/components/layout/left-sidebar.tsx
@@ -4,19 +4,18 @@
 import React from 'react';
 import { FriendsNavigationItem } from '@/components/friends/friends-navigation-item';
 import { RoomList } from '@/components/rooms/room-list';
-// Removed: import { DirectMessageList } from '@/components/dms/direct-message-list';
 import { CreateRoomDialog } from '@/components/rooms/create-room-dialog';
 import { Separator } from '@/components/ui/separator';
 
 export function LeftSidebar() {
   return (
     <div className="flex flex-col h-full">
-      {/* Friends Section - Hidden on mobile (screens smaller than md), block on md and larger */}
-      <div className="hidden p-4 md:block">
+      {/* Friends Section - Now always visible */}
+      <div className="p-4">
         <h2 className="text-lg font-semibold font-headline mb-2">Friends</h2>
         <FriendsNavigationItem />
       </div>
-      <Separator className="hidden md:block" />
+      <Separator /> {/* Separator is now always visible */}
 
       {/* Rooms Section - Always Visible */}
       <div className="p-4">
@@ -28,8 +27,6 @@ export function LeftSidebar() {
       <div className="flex-grow overflow-y-auto"> {/* RoomList should take remaining space */}
          <RoomList />
       </div>
-
-      {/* Direct Messages Section Removed from here */}
     </div>
   );
 }
